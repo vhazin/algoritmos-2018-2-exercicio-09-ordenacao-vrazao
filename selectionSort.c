276d1031195bde7687cc889b436caa99cfd62aa1
Jonathan Coutinho A Santos <37001764+jsantos93@users.noreply.github.com>
2018-11-25 13:47:43 -0300

    Add comments

--- selectionSort.c
+++ selectionSort.c
@@ -36,13 +36,13 @@ void selectionSort(int *arr, int n)
 {
     int i, j, min_idx;
 
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < n - 1; i++) 
     {
         min_idx = i;
-        for (j = i + 1; j < n; j++)
+        for (j = i + 1; j < n; j++) // varre o array procurando o menor valor e compara com o indice i
             if (arr[j] < arr[min_idx])
                 min_idx = j;
 
-        swap(&arr[min_idx], &arr[i]);
+        swap(&arr[min_idx], &arr[i]); // Caso min_idx != i, fazer a troca.
     }
 }
