cb2fd793cde5d88d0af5d3f65d276edb5fa49c60
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-11-26 09:21:12 -0300

    Update mergeSort.c

--- mergeSort.c
+++ mergeSort.c
@@ -35,32 +35,32 @@ void mergeSort(int *a, int l, int h)
         }
     }
     else if (h == l)
-        return;
+        return; // Condição de parada unitária
     else if (h > l)
     {
         int size = h - l + 1;
         int m = l + (h - l) / 2;
         mergeSort(a, l, m);
-        mergeSort(a, m + 1, h);
+        mergeSort(a, m + 1, h); // Até aqui ele saiu quebrando o vetor em pedaços menores
         int b[size];
         int k = 0;
         int i = l;
         int j = m + 1;
         while (i <= m && j <= h)
-            if (a[i] <= a[j])
-                b[k++] = a[i++];
+            if (a[i] <= a[j]) // Se o da esquerda for menor
+                b[k++] = a[i++]; // Joga o da esquerda em vez do da direita
             else
-                b[k++] = a[j++];
+                b[k++] = a[j++]; // Se nao joga o da direita
 
         while (i <= m)
-            b[k++] = a[i++];
+            b[k++] = a[i++]; // Aqui o restante do array ja ta ordenado então é só copiar
 
         while (j <= h)
-            b[k++] = a[j++];
+            b[k++] = a[j++]; // E depois copiar o restante ordenado da direita
 
         for (k = 0; k < size; k++)
         {
-            a[l + k] = b[k];
+            a[l + k] = b[k]; // Agora passa do auxiliar pro principal
         }
     }
 }
